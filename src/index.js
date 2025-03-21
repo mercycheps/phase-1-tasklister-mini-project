7d3e34ba3eb16dc8f9785dccfc6a0a90e8d5e24c
Mercy <maritimmercy8@gmail.com>
2025-03-21 20:14:44 +0300

    added delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener( `DOMContentLoaded`,() => {
+  const form = document.querySelector(`form`)
+  form.addEventListener(`submit`,(event) => {
+      console.log(event)
+      buildToDO(event.target["new-task-description"].value)
+      form.reset()
+      
+  })
+ 
+  
+
+})
+function buildToDO(todo) {
+  let ul = document.getElementById(`tasks`)
+  let button = document.createElement(`button`)
+  button.addEventListener(`click`,handleDelete)
+   button.textContent = `X`
+  const li = document.createElement("li")
+  li.textContent = todo
+  li.appendChild(button)
+   ul.appendChild(li)
+}
+
+  function handleDelete(e) {
+      e.target.parentNode.remove()
+  }
+
